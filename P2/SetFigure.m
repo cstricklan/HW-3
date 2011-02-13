91163a47cfb72ef05f5b372b43f612a52419bbf6
cstricklan <contact@kraetonics.com>
2011-02-13 00:12:17 -0500

    Removed Uneeded Files

--- P2/SetFigure.m
+++
@@ -1,7 +0,0 @@
-
-function SetFigure( Figure, Name, Position )
-  set(Figure, 'color', 'w');
-  set(Figure, 'Name', Name);
-  set(Figure, 'NumberTitle', 'off');
-  set(Figure, 'Position', Position);
-end
